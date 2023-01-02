d5fcf45394edcb22e6d9a9a5fd925585021039ec
Damir Aubekerov <80846714+Yaksat@users.noreply.github.com>
2023-01-02 11:10:18 +0400

    first commit

---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-48 mx-auto">
+        <h1 class="text-lg">Posts</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
