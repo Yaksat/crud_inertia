40da1cef1fc2b595c5e9edcddb27c39100e5a03a
Damir Aubekerov <80846714+Yaksat@users.noreply.github.com>
2023-01-02 12:46:45 +0400

    add store posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Post\StoreRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -15,4 +17,13 @@ class PostController extends Controller
     {
         return inertia('Post/Create');
     }
+
+    public function store(StoreRequest $request)
+    {
+        Post::create($request->validated());
+
+        return redirect()->route('post.index');
+    }
+
+
 }
