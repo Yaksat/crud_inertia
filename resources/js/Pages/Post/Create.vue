464b4f5df1756a39d819b3221dd02031f71dc15b
Damir Aubekerov <80846714+Yaksat@users.noreply.github.com>
2023-01-02 12:34:40 +0400

    edit form for create post

--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="w-96 mx-auto pt-8">
         <h1 class="text-lg mb-8">Create</h1>
-        <form>
+        <form @submit.prevent="store">
             <div class="mb-4">
-                <input class="w-full rounded-full border-gray-400" type="text" placeholder="title">
+                <input v-model="title" class="w-full rounded-full border-gray-400" type="text" placeholder="title">
+            </div>
+            <div class="mb-4">
+                <textarea v-model="content" class="w-full rounded-full border-gray-400" placeholder="content"></textarea>
             </div>
             <div>
-                <textarea class="w-full rounded-full border-gray-400" placeholder="content"></textarea>
-            </div>
-            <div>
-                <button class="hover:bg-white hover:text-sky-500 border border-sky-500 block text-white p-2 bg-sky-500 rounded-full text-center w-32" type="submit">Store</button>
+                <button class="ml-auto hover:bg-white hover:text-sky-500 border border-sky-500 block text-white p-2 bg-sky-500 rounded-full text-center w-32" type="submit">Store</button>
             </div>
         </form>
     </div>
@@ -22,6 +22,19 @@ export default {
     name: "Index",
     components: {
         Link
+    },
+
+    data() {
+      return {
+          title: '',
+          content: '',
+      }
+    },
+
+    methods: {
+        store() {
+            this.$inertia.post('/posts', {title: this.title, content: this.content})
+        }
     }
 }
 </script>
